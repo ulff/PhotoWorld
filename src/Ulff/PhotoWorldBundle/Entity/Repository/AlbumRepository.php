11c3509aac0c85f289550808637226150d95acec
Olaf Gałązka <ulff.pl@gmail.com>
2014-12-14 19:16:37 +0100

    merged last svn changes

--- src/Ulff/PhotoWorldBundle/Entity/Repository/AlbumRepository.php
+++ src/Ulff/PhotoWorldBundle/Entity/Repository/AlbumRepository.php
@@ -3,6 +3,7 @@
 namespace Ulff\PhotoWorldBundle\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr;
 
 /**
  * AlbumRepository
@@ -14,8 +15,22 @@ class AlbumRepository extends EntityRepository {
     
     public function getAlbumList($params = array()) {
         $qb = $this->createQueryBuilder('a');
-        $qb->select('a');
+
+        $qb->select(array(
+            'a.id',
+            'a.title',
+            'a.createdate',
+            'a.createdby',
+            'u.id AS uid',
+            'u.lastname',
+            'u.firstname',
+            'COUNT(p.id) AS total'
+        ));
+
+        $qb->leftJoin('a.photos', 'p');
+        $qb->leftJoin('Ulff\UserBundle\Entity\User', 'u', Expr\Join::WITH, 'a.createdby = u.id');
         $qb->addOrderBy('a.createdate', 'DESC');
+        $qb->groupBy('a.id');
 
         if (!empty($params['limit'])) {
             $qb->setMaxResults($params['limit']);
