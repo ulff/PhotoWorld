c1fad3452fdff1be78409baaed5ff719d9a14b2e
Olaf Gałązka <ulff.pl@gmail.com>
2015-02-09 15:43:34 +0100

    added Like entity

---
+++ src/Ulff/PhotoWorldBundle/Entity/Like.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Ulff\PhotoWorldBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Description of Like
+ *
+ * @author ulff
+ *
+ * @ORM\Entity(repositoryClass="Ulff\PhotoWorldBundle\Entity\Repository\LikeRepository")
+ * @ORM\Table(name="like")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Like
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Photo", inversedBy="likes")
+     * @ORM\JoinColumn(name="photoid", referencedColumnName="id")
+     */
+    protected $photo;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Ulff\UserBundle\Entity\User", inversedBy="likes")
+     * @ORM\JoinColumn(name="userid", referencedColumnName="id")
+     */
+    protected $user;
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $photo
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+}
